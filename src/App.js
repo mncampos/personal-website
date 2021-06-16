5c22b77b6f4e428715e7b65d183d46a73796042b
mncampos <mncampos@inf.ufrgs.br>
2021-06-16 02:00:45 -0300

    Basic website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {SocialIcon} from 'react-social-icons';
+import pdf from "./Curriculo.pdf";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<img src= "https://media-exp1.licdn.com/dms/image/D4E35AQFPq8kFAcKyEA/profile-framedphoto-shrink_200_200/0/1623275060171?e=1623906000&v=beta&t=Zx4hTXo8cPY0cYDe0vGvcshAfwOXH5B8jCyNXerVDkg" className="photo-circle" alt="mncampos"/>
+      <div className="info-box">
+        <h1 className="myName">Mateus Nunes Campos</h1>
+        <h2 className="subtitle">Computer Science Student</h2>
+        <h3 className="email">mncampos@inf.ufrgs.br</h3>
+        <SocialIcon url="https://www.linkedin.com/in/mateus-nunes-campos-8a1748214/"/>
+        <SocialIcon url="https://github.com/mncampos"/>
+        <SocialIcon url="https://api.whatsapp.com/send?phone=5551995925737"/>
+        <SocialIcon url={pdf} label="Curriculo" bgColor="#ff5a01"/>
+
+       
+      </div>
     </div>
+    
   );
 }
 
